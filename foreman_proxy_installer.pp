f4992e6dc80f109356142f97cdcbc5919413d4c4
Ivan Necas <inecas@redhat.com>
2013-02-01 14:46:52 +0100

    Update foreman_proxy_installer.pp

--- foreman_proxy_installer.pp
+++ foreman_proxy_installer.pp
@@ -1,6 +1,9 @@
 class { foreman_proxy:
+  servername => "192.168.100.1",
+  port => 9090,
+  puppetca => false,
   dhcp => true,
-  dhcp_interface => "eth1",
+  dhcp_interface => "virbr1",
   dns => true,
-  dns_interface => "eth1",
+  dns_interface => "virbr1",
   tftp => true }
