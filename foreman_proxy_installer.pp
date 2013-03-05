d2b48a74911af9803b630fc4c09424aefc8794c1
Ivan Necas <necasik@gmail.com>
2013-03-05 10:08:59 +0100

    Remove pp file: cli is there already

--- foreman_proxy_installer.pp
+++
@@ -1,9 +0,0 @@
-class { foreman_proxy:
-  servername => "192.168.100.1",
-  port => 9090,
-  puppetca => false,
-  dhcp => true,
-  dhcp_interface => "virbr1",
-  dns => true,
-  dns_interface => "virbr1",
-  tftp => true }
